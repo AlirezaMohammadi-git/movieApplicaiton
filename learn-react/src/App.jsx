9c4acdbe7222f9cb898e7c9540d6d35da7916a4b
Joe <a.m.sadra4@gmail.com>
2025-04-27 06:04:45 +0330

    movies section implemented

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,16 +1,16 @@
 
 import heroImage from './assets/hero.png'
-import heroBackground from './assets/hero-bg.png'
 import { SearchBox } from './components/Search.jsx'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { Spinner } from './components/spinner.jsx'
+import { MovieCard } from './components/MovieCard.jsx'
+import { useDebounce } from 'use-debounce'
 
-export const Header = () => {
 
-  const [searchTerm, setSearchTerm] = useState("");
-
+const Header = ({ searchTerm, setSearchTerm }) => {
   return (
     <main>
-      <img src={heroBackground} className='pattern' />
+      <div className='pattern' />
 
       <div className="wrapper">
         <header>
@@ -25,10 +25,84 @@ export const Header = () => {
   )
 }
 
+export const AllMovies = ({ isLoading, movieList, errorMessage }) => {
+  return (<section className='all-movies'>
+    <h2>All Movies</h2>
+    {
+      isLoading ?
+        (<Spinner />) :
+        errorMessage ?
+          (<p className='text-red-500'>{errorMessage}</p>) :
+          (<ul>
+            {movieList.map(movie => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
+          </ul>)
+    }
+
+  </section>)
+}
+
 export const App = () => {
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [movieList, setMovieList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [debounceSearchTerm] = useDebounce(searchTerm, 1000);
+  const BASE_URL = "https://api.themoviedb.org/3";
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const API_OPTIONS = {
+    method: "GET",
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${API_KEY}`
+    }
+  }
+
+  async function fetchMovies(query = '') {
+
+    const endpoint = query
+      ? `${BASE_URL}/search/movie?query=${encodeURIComponent(query)}&include_adult=true`
+      : `${BASE_URL}/discover/movie?include_adult=true&include_video=true&language=en-US&page=1&sort_by=popularity.desc`;
+    setIsLoading(true);
+    setErrorMessage('');
+
+    try {
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("failed to fetch data");
+      }
+
+      const data = await response.json();
+      if (data.Response === "False") {
+        setErrorMessage(data.error) || "Failed to load movies";
+        setMovieList([])
+        return;
+      }
+
+      setMovieList(Array.from(data.results))
+
+    } catch (error) {
+      setErrorMessage('Failed to Load movies. Please try again later!')
+      console.log(`Error while fetching movies in App.jsx : ${error}`)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+
+  // below will run only one time! because of empty dependencies.
+  useEffect(() => {
+    fetchMovies(debounceSearchTerm)
+  }, [debounceSearchTerm])
+
   return (
     <>
-      <Header />
+      <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <AllMovies isLoading={isLoading} movieList={movieList} errorMessage={errorMessage} />
     </>
   )
 }
---
+++ learn-react/src/components/MovieCard.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import starImage from '../assets/star.svg'
+import noMovieImage from "../assets/no-movie.png"
+
+export const MovieCard = ({ movie: { title, poster_path, vote_average, original_language, release_date } }) => {
+    return (
+        <div className="movie-card">
+            <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : noMovieImage} alt="" />
+            <div className="mt-4" />
+            <h3>{title}</h3>
+            <div className="content">
+                <div className="rating">
+                    <img src={starImage} alt="star" />
+                    <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+                    <span>•</span>
+                    <p className="lang">{original_language}</p>
+                    <span>•</span>
+                    <p className="year">{release_date ? release_date.split('-')[0] : "N/A"}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
