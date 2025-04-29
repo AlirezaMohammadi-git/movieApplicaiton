12fffb1be2830aba36ea00ad17e3b8ca1080f3a5
Joe <a.m.sadra4@gmail.com>
2025-04-30 02:18:43 +0330

    new : Floating Search while scrolling

--- learn-react/src/components/Search.jsx
+++ learn-react/src/components/Search.jsx
@@ -1,19 +1,59 @@
-import React, { useContext } from "react"
+import React, { useContext, useRef } from "react"
 import searchIcon from '../assets/search.svg'
 import { GlobalStateContext } from "../States"
 
 export const SearchBox = () => {
 
     const { searchTerm, setSearchTerm } = useContext(GlobalStateContext)
-    // Don't change searchTerm itself,use setSearchTerm to change it (otherwise react will break!)
-    // incorrect : searchTerm = "new value";
-    // correct : setSearchTerm('new value')
+    const floatingSearch = useRef(null);
+    const search = useRef(null)
+
+    document.addEventListener('scrollend', () => {
+        const cordinates = search.current.getBoundingClientRect();
+        console.log()
+        if (cordinates.bottom > 0) {
+            floatingSearch.current.classList.remove('active')
+            setTimeout(() => {
+                if (!floatingSearch.current.classList.contains('active')) {
+                    floatingSearch.current.classList.add('invisible')
+                }
+            }, 400);
+        } else {
+            floatingSearch.current.classList.remove('invisible')
+            setTimeout(() => {
+                if (!floatingSearch.current.classList.contains('invisible')) {
+                    floatingSearch.current.classList.add('active')
+                }
+            }, 400)
+        }
+    })
+
     return (
-        <div className="search">
-            <div>
-                <img src={searchIcon} alt="Search icon" />
-                <input type="text" placeholder="Search Movie Name" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
+        <>
+
+            <div className="search-float invisible" ref={floatingSearch}>
+                <div>
+                    <img src={searchIcon} alt="Search icon" />
+                    <input
+                        type="text"
+                        placeholder="Search Movie Name"
+                        value={searchTerm}
+                        onChange={(event) => setSearchTerm(event.target.value)} />
+                </div>
             </div>
-        </div>
+
+
+            <div className="search" ref={search}>
+                <div>
+                    <img src={searchIcon} alt="Search icon" />
+                    <input
+                        type="text"
+                        placeholder="Search Movie Name"
+                        value={searchTerm}
+                        onChange={(event) => setSearchTerm(event.target.value)} />
+                </div>
+            </div>
+
+        </>
     )
 }
