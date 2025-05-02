fd067692064be71b5cb5ffefd5992152259b4922
Joe <a.m.sadra4@gmail.com>
2025-05-02 11:12:41 +0330

    -fix: main search didn't focus after searching on floatin search. -fix: using IntersectionObserver instead of scroll event for better performance

--- learn-react/src/components/Search.jsx
+++ learn-react/src/components/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from "react"
+import React, { useContext, useEffect, useRef } from "react"
 import searchIcon from '../assets/search.svg'
 import { GlobalStateContext } from "../States"
 
@@ -7,25 +7,33 @@ export const SearchBox = () => {
     const { searchTerm, setSearchTerm } = useContext(GlobalStateContext)
     const floatingSearch = useRef(null);
     const search = useRef(null)
+    const observer = useRef()
 
-    document.addEventListener('scrollend', () => {
-        const cordinates = search.current.getBoundingClientRect();
-        if (cordinates.bottom > 0) {
-            floatingSearch.current.classList.remove('active')
-            setTimeout(() => {
-                if (!floatingSearch.current.classList.contains('active')) {
-                    floatingSearch.current.classList.add('invisible')
-                }
-            }, 400);
-        } else {
-            floatingSearch.current.classList.remove('invisible')
-            setTimeout(() => {
-                if (!floatingSearch.current.classList.contains('invisible')) {
-                    floatingSearch.current.classList.add('active')
-                }
-            }, 400)
-        }
-    })
+    useEffect(() => {
+        if (observer.current) observer.current.disconnect();
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting) {
+                // main search bar is visible, so disabling floating search
+                floatingSearch.current.classList.remove('active')
+                search.focus();
+                setTimeout(() => {
+                    if (!floatingSearch.current.classList.contains('active')) {
+                        floatingSearch.current.classList.add('invisible')
+                    }
+                }, 400);
+            } else {
+                floatingSearch.current.classList.remove('invisible')
+                setTimeout(() => {
+                    if (!floatingSearch.current.classList.contains('invisible')) {
+                        floatingSearch.current.classList.add('active')
+                    }
+                }, 400)
+            }
+        })
+
+        if (search) observer.current.observe(search.current)
+    }, [])
+
 
     return (
         <>
